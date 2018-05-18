e5fbe311fe1656262d1c22c2122ac6f57f79ccc0
mhf <mhfcbz@163.com>
2018-05-18 14:02:11 +0800

    s:

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+#![allow(dead_code)]
+
+extern crate glium;
 extern crate libc;
+
+use glium::glutin;
 use libc::c_int;
 
 extern "C" {
@@ -6,6 +11,29 @@ extern "C" {
 }
 
 fn main() {
+  gl();
+}
+
+fn gl() {
+  let mut events_loop = glutin::EventsLoop::new();
+  let window = glutin::WindowBuilder::new();
+  let context = glutin::ContextBuilder::new();
+  let display = glium::Display::new(window, context, &events_loop).unwrap();
+
+  let mut closed = false;
+  while !closed {
+    events_loop.poll_events(|ev| match ev {
+      glutin::Event::WindowEvent { event, .. } => match event {
+        glutin::WindowEvent::Closed => closed = true,
+        _ => (),
+      },
+      _ => (),
+    });
+  }
+}
+
+// c ffi
+fn run_sum() {
   let a = 1;
   let b = 2;
   let c = unsafe { sum(a, b) };
