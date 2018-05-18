63ac192c0227e4463df5633e42090fd2554eadf0
mhf <mhfcbz@163.com>
2018-05-18 15:45:34 +0800

    s:

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![allow(dead_code)]
+#[macro_use]
 
 extern crate glium;
 extern crate libc;
 
-use glium::glutin;
+use glium::{glutin, Surface};
 use libc::c_int;
 
 extern "C" {
@@ -14,14 +15,71 @@ fn main() {
   gl();
 }
 
+#[derive(Copy, Clone)]
+struct Vertex {
+  position: [f32; 2],
+}
+
 fn gl() {
   let mut events_loop = glutin::EventsLoop::new();
   let window = glutin::WindowBuilder::new();
   let context = glutin::ContextBuilder::new();
   let display = glium::Display::new(window, context, &events_loop).unwrap();
 
+  implement_vertex!(Vertex, position);
+
+  let vertext1 = Vertex {
+    position: [-0.5, -0.5],
+  };
+  let vertext2 = Vertex {
+    position: [0.0, 0.5],
+  };
+  let vertext3 = Vertex {
+    position: [0.5, -0.25],
+  };
+  let shape = vec![vertext1, vertext2, vertext3];
+
+  let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+  let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+  let vertex_shader_src = r#"
+    #version 140
+
+    in vec2 position;
+
+    void main() {
+      gl_Position = vec4(position, 0.0, 1.0);
+    }
+  "#;
+
+  let fragment_shader_src = r#"
+    #version 140
+
+    out vec4 color;
+
+    void main() {
+      color = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+  "#;
+
+  let program =
+    glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
   let mut closed = false;
   while !closed {
+    let mut target = display.draw();
+    target.clear_color(0.0, 0.0, 1.0, 1.0);
+    target
+      .draw(
+        &vertex_buffer,
+        &indices,
+        &program,
+        &glium::uniforms::EmptyUniforms,
+        &Default::default(),
+      )
+      .unwrap();
+    target.finish().unwrap();
+
     events_loop.poll_events(|ev| match ev {
       glutin::Event::WindowEvent { event, .. } => match event {
         glutin::WindowEvent::Closed => closed = true,
